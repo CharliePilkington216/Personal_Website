02ffb6fbc5ceca74869892f5e04eca539758ebf5
CharliePilkington216 <pilkington216@gmail.com>
2026-08-16 11:46:36 +0100

    api and json types defined

---
+++ src/Authorization.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+-- module to organise the /auth section of the api
+module Authorization where
+
+import Data.Text (Text)
+import GHC.Generics (Generic)
+import Data.Aeson (FromJSON, ToJSON)
+import Servant
+import Web.Cookie (SetCookie)
+
+-- Request JSON definitions
+
+data LogInRequest = LogInRequest
+  { email :: Text
+  , password :: Text
+  } deriving (Show, Generic) 
+
+instance FromJSON LogInRequest
+
+-- Response JSON defintions
+
+data AccessToken = AccessToken
+  { token :: Text
+  } deriving (Show, Generic)
+
+instance ToJSON AccessToken
+
+type LogInResponse = AccessToken
+
+type RefreshResponse = AccessToken
+
+
+-- API type definition
+
+type AuthAPI = "login" 
+                :> ReqBody '[JSON] LogInRequest 
+                :> Post '[JSON] (Headers '[Header "Set-Cookie" SetCookie] LogInResponse)
+          :<|> "refresh" 
+                :> Header' '[Required, Strict] "Cookie" Text 
+                :> Post '[JSON] RefreshResponse
+          :<|> "logout" 
+                :> Header' '[Required, Strict] "Cookie" Text 
+                :> PostNoContent '[JSON] (Headers '[Header "Set-Cookie" SetCookie] NoContent)
