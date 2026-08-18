6f8dddae2ba0019a95d100afa754b9708d14c4b3
CharliePilkington216 <pilkington216@gmail.com>
2026-08-18 12:25:40 +0100

    .cabal updated and imports modified

--- src/Portfolio.hs
+++ src/Portfolio.hs
@@ -10,31 +10,11 @@
 
 module Portfolio where
 
-import Prelude ()
-import Prelude.Compat
-
-import Control.Monad.Except
-import Control.Monad.Reader
-import Data.Aeson
-import Data.Aeson.Types
-import Data.Attoparsec.ByteString
-import Data.ByteString (ByteString)
-import Data.List
-import Data.Maybe
-import Data.String.Conversions
-import Data.Time.Calendar
-import GHC.Generics
-import Lucid
-import Network.HTTP.Media ((//), (/:))
-import Network.Wai
-import Network.Wai.Handler.Warp
+import Data.Aeson (ToJSON(..), FromJSON(..), genericToJSON, genericParseJSON, defaultOptions, camelTo2)
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+import GHC.Generics (Generic)
 import Servant
-import System.Directory
-import Text.Blaze
-import Text.Blaze.Html.Renderer.Utf8
-import Servant.Types.SourceT (source)
-import qualified Data.Aeson.Parser
-import qualified Text.Blaze.Html
 
 -- JSON definitions for the Portfolio API
 
