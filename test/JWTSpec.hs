79e2fa46f85c6f698bcea2ca95e4cdc790f3372c
CharliePilkington216 <pilkington216@gmail.com>
2026-08-18 20:43:04 +0100

    token expiration denial test added

--- test/JWTSpec.hs
+++ test/JWTSpec.hs
@@ -28,11 +28,13 @@ refreshRequest token =
           ]
       }
 
-testSecret, testDomain, testAdminId, testSessionId :: Text
+testSecret, testDomain, testAdminId, testSessionId, expiredAccessToken, expiredRefreshToken :: Text
 testSecret   = "test-jwt-secret-for-unit-tests-only"
 testDomain   = "example.com"
 testAdminId  = "3fa85f64-5717-4562-b3fc-2c963f66afa6"
 testSessionId = "9c858901-8a57-4791-81fe-4c455b099bc9"
+expiredAccessToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJleGFtcGxlLmNvbSIsImV4cCI6MS43ODcwODIwOTYxMjgwODE1NzNlOSwiaWF0IjoxLjc4NzA4MTE5NjEyODA4MTU3M2U5LCJpc3MiOiJleGFtcGxlLmNvbSIsInNlc3Npb24iOiI5Yzg1ODkwMS04YTU3LTQ3OTEtODFmZS00YzQ1NWIwOTliYzkiLCJzdWIiOiIzZmE4NWY2NC01NzE3LTQ1NjItYjNmYy0yYzk2M2Y2NmFmYTYiLCJ0eXBlIjoiYWNjZXNzIn0.14HTPIByePY7k5WuhGs1-NNsuFu3FCt5lPDyGaZVrs4"
+expiredRefreshToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJleGFtcGxlLmNvbSIsImV4cCI6MS43ODcwNzc4MTA3NDY3MDQ0N2U5LCJpYXQiOjEuNzg2NDczMDEwNzQ2NzA0NDdlOSwiaXNzIjoiZXhhbXBsZS5jb20iLCJzZXNzaW9uIjoiOWM4NTg5MDEtOGE1Ny00NzkxLTgxZmUtNGM0NTViMDk5YmM5Iiwic3ViIjoiM2ZhODVmNjQtNTcxNy00NTYyLWIzZmMtMmM5NjNmNjZhZmE2IiwidHlwZSI6InJlZnJlc2gifQ.SvJn6RDN8BT684ckCuEo7_5xu-M2EpicBiJkDwI1Mfk"
 
 spec :: Spec
 spec = do
@@ -62,11 +64,19 @@ spec = do
       verified <- verifyAccessToken testSecret testDomain tampered
       verified `shouldSatisfy` isLeft
 
-  describe "createRefreshToken / verifyRefreshToken" $
+    it "rejects an expired token" $ do
+      verified <- verifyAccessToken testSecret testDomain expiredAccessToken
+      verified `shouldSatisfy` isLeft
+
+  describe "createRefreshToken / verifyRefreshToken" $ do
     it "round-trips the same way as access tokens" $ do
       Right token <- createRefreshToken testSecret testDomain testAdminId testSessionId
       verified <- verifyRefreshToken testSecret testDomain token
       verified `shouldBe` Right (testAdminId, testSessionId)
+
+    it "rejects an expired token" $ do
+      verified <- verifyRefreshToken testSecret testDomain expiredRefreshToken
+      verified `shouldSatisfy` isLeft
 
   describe "token type enforcement" $ do
     it "rejects an access token where a refresh token is expected" $ do
@@ -80,7 +90,6 @@ spec = do
       verified `shouldSatisfy` isLeft
 
   describe "adminAuthHandler" $ do
-
     it "returns the admin/session ids when given a valid access token" $ do
       Right accessToken <-
         createAccessToken
